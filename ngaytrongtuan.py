84160d3e11121d7e0a22168d78379e8cf7f3e845
DADAHN <longnh9795@gmail.com>
2022-10-31 11:32:18 +0700

    ngaytrongtuan

---
+++ ngaytrongtuan.py
@@ -0,0 +1,18 @@
+ngay=int(input('Nhập số từ 1 đến 7:'))
+if 1<=ngay<=7:
+	if ngay==1:
+		print('Hôm nay là thứ 2')
+	if ngay==2:
+		print('Hôm nay là thứ 3')
+	if ngay==3:
+		print('Hôm nay là thứ 4')
+	if ngay==4:
+		print('Hôm nay là thứ 5')
+	if ngay==5:
+		print('Hôm nay là thứ 6')
+	if ngay==6:
+		print('Hôm nay là thứ 7')
+	if ngay==7:
+		print('Hôm nay là chủ nhật')
+else:
+	print('Vui lòng nhập lại!')
